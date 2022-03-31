4f669f8c492a5df51f32e8cfaa900404251dac5e
tomasyarce2007 <a>
2022-03-31 15:23:08 -0500

    Se finaliza todo

--- epm.js
+++ epm.js
@@ -8,8 +8,25 @@ function recibirDatos (event){
     let estrato = document.getElementById("selector").value
 
 
-    if(estrato== "1"){
-        alert("usted tiene un descuento del 40%")
-        alert("su valor a pagar es de $" + valor*0.4)
+    switch(estrato) {
+        case "1" : alert("Usted tiene un descuento del 40% por lo que su valor a pagar sera de: $" + valor*0.6)
+        break
+
+        case "2" : alert("Usted tiene un descuento del 30% por lo que su valor a pagar sera de: $" + valor*0.7)
+        break
+
+        case "3" : alert("Usted tiene un descuento del 20% por lo que su valor a pagar sera de: $" + valor*0.8)
+        break
+
+        case "4" : alert("Usted no tiene un descuento, por lo que su valor a pagar sera de: $" + valor)
+        break
+
+        case "5" : alert("Usted no tiene un descuento, por lo que su valor a pagar sera de: $" + valor)
+        break
+
+        case "6" : alert("Usted no tiene un descuento, por lo que su valor a pagar sera de: $" + valor)
+        break
+
+        default : alert("Opción no valida")
     }
-}
+    }
